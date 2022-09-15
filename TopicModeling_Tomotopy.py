126222cd8b809f6ddbe57ea4f857b6b7f42dc982
woomin nam <applewoods9559@gmail.com>
2022-09-15 18:09:44 +0900

    update parameter

--- TopicModeling_Tomotopy.py
+++ TopicModeling_Tomotopy.py
@@ -6,12 +6,6 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
-# Preprocessing
-import gensim
-from gensim import corpora
-from gensim.models import TfidfModel, CoherenceModel
-from gensim.test.utils import common_corpus
-
 # LDA Library
 import tomotopy as tp
 
@@ -22,20 +16,23 @@ class Tomotopy_LDA_Models:
     def __init__(self, tokenized_doc, path='./'):
         self.path = path
         self.tokenized_doc = tokenized_doc
-        self.dictionary = corpora.Dictionary(tokenized_doc)
 
-        # Bag of Words corpus
-        self.corpus = [self.dictionary.doc2bow(text) for text in tokenized_doc]
-
-        # TF-IDF corpus
-        tfidf = TfidfModel(self.corpus)
-        self.corpus_tfidf = tfidf[self.corpus]
+        # LDA Parameter
+        self.num_topics = 10
+        self.min_cf = 10
+        self.tw = 'IDF'
+        self.rm_top = 0
+        self.alpha = 0.1
+        self.eta = 0.01
+        self.seed = np.int64(datetime.now().strftime('%Y'))
 
         # etc
         self.today = datetime.now().strftime('%Y%m%d')
         self.save_file_name = np.int64(self.today + '001')
+        self.show_detail = False
+        self.model_index = 0                                     # 여러개의 LDA 결과 중 해당 결과를 볼 때 사용하는 변수
 
-    def LDA(self, num_topics= 10, min_cf= 10, tw= 'IDF'):
+    def LDA(self, num_topics = 10):
         # num_topic : 토픽의 갯수
         # min_df : 
         # min_cf : 전체 말뭉치에서 N회 미만 등장한 단어들 제거
@@ -43,26 +40,32 @@ class Tomotopy_LDA_Models:
         # IDF(정보량) : 해당 사건이 발생한 확률의 역수에 로그를 취한 값
         # PMI(점별 상호정보량) : 문헌이 출현할 확률과 단어가 출현할 확률의 
 
+        self.num_topics = num_topics
+
         termweight = ''
-        if tw == 'ONE':
+        if self.tw == 'ONE':
             termweight = tp.TermWeight.ONE
-        elif tw == 'IDF':
+        elif self.tw == 'IDF':
             termweight = tp.TermWeight.IDF
-        elif tw == 'PMI':
+        elif self.tw == 'PMI':
             termweight = tp.TermWeight.PMI
         
-        self.LDA_model = tp.LDAModel(k= num_topics, min_cf= min_cf, tw= termweight, rm_top= 0, alpha= 0.1, eta= 0.01, seed= 2022)
+        self.LDA_model = tp.LDAModel(k= self.num_topics, min_cf= self.min_cf, tw= termweight, rm_top= self.rm_top, alpha= self.alpha, eta= self.eta, seed= self.seed)
 
         for token in self.tokenized_doc:
             self.LDA_model.add_doc(token)
 
-    def train_model(self, max= 500, term= 100):
+    def train_model(self):
+        # 초기화
         self.LDA_model.train(0)
-        self.show_model_detail()
+
+        if self.show_detail:
+            self.show_model_detail()
 
         print('Training...', flush= True)
-        for i in range(0, max, term):
-            self.LDA_model.train(term)
+
+        for i in range(0, self.max_train, self.term_train):
+            self.LDA_model.train(self.term_train)
             print('Iteration: {}\t Log-likelihood: {}'. format(i, self.LDA_model.ll_per_word))
 
     def show_model_detail(self):
@@ -88,14 +91,14 @@ class Tomotopy_LDA_Models:
 
         return perplexity_score
 
-    def compute_LDA_Models(self, min_topics= 2, max_topics= 100, steps= 1):
+    def compute_LDA_Models(self):
         self.coherence_values = list()
         self.perplexity_values = list()
         self.lda_model_list = list()
 
-        for num_topic in range(min_topics, max_topics, steps):
+        for num_topic in range(self.min_topics, self.max_topics, self.steps):
             self.LDA(num_topics= num_topic)
-            self.train_model(max= 500, term= 100)
+            self.train_model()
 
             self.lda_model_list.append(self.LDA_model)
             self.coherence_values.append(self.get_coherenece())
@@ -104,11 +107,26 @@ class Tomotopy_LDA_Models:
         max_cohernece = max(self.coherence_values)
         self.max_index = self.coherence_values.index(max_cohernece)
 
-    def optimal_number_of_topics(self, min_topics= 2, max_topics= 100, steps= 1):
-        self.compute_LDA_Models(min_topics= min_topics, max_topics= max_topics, steps= steps)
+    def optimal_number_of_topics(self, min_topics= 2, max_topics= 100, steps= 1, min_cf= 10, tw= 'IDF', rm_top= 0, alpha= 0.1, eta= 0.01, max_train= 500, term_train= 100, show_detail= False):
+        self.min_topics = min_topics
+        self.max_topics = max_topics
+        self.steps = steps
+
+        self.min_cf = min_cf
+        self.tw = tw
+        self.rm_top = rm_top
+        self.alpha = alpha
+        self.eta = eta
+    
+        self.max_train = max_train
+        self.term_train = term_train
+
+        self.show_detail = show_detail
+
+        self.compute_LDA_Models()
 
         # Coherence Visualization
-        x = range(min_topics, max_topics, steps)
+        x = range(self.min_topics, self.max_topics, self.steps)
         plt.figure(figsize=(16, 8))
         plt.plot(x, self.coherence_values)
         plt.title('Coherence')
@@ -145,8 +163,8 @@ class Tomotopy_LDA_Models:
 
         return lda_topics_list, dataframe
 
-    def show_topics(self, model_index= 0, top_n= 20, show_n= 5):
-        model = self.lda_model_list[model_index]
+    def show_topics(self, top_n= 20, show_n= 5):
+        model = self.lda_model_list[self.model_index]
         topic_list, df = self.make_dataframe(model= model, top_n= top_n)
         
         for topic in topic_list:
@@ -154,6 +172,7 @@ class Tomotopy_LDA_Models:
             print(', '.join(w for w in topic[1: show_n + 1]))
 
         self.save_excel(dataframe= df)
+        self.save_model(model= model)
 
     def save_excel(self, dataframe):
         CHECK = True
@@ -169,8 +188,16 @@ class Tomotopy_LDA_Models:
             else:
                 self.save_file_name += 1
 
-    def best_result(self, top_n= 20):
-        self.show_topics(model_index= self.max_index, top_n= top_n, show_n= 5)
+    def save_model(self, model):
+        FILE_NAME = 'LDA_Model_{}.bin'.format(self.save_file_name)
+        FILE_PATH = self.path + 'Save_Model/'
 
-    def show_result(self, index= 0, top_n= 20):
-        self.show_topics(model_index= index, top_n= top_n, show_n= 5)
+        model.save(FILE_PATH + FILE_NAME)
+
+    def best_result(self, top_n= 20, show_n = 5):
+        self.model_index = self.max_index
+        self.show_topics(top_n= top_n, show_n= show_n)
+
+    def show_result(self, topic_choice= 0, top_n= 20, show_n = 5):
+        self.model_index = topic_choice - self.min_topics
+        self.show_topics(top_n= top_n, show_n= show_n)
